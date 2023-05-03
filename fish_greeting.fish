2cff8a4d15c367c7856fd75c11a59f2303a6a047
Fantastic Mr. Fox <87468088+gadefox@users.noreply.github.com>
2023-05-03 12:36:40 +0200

    Add files via upload

---
+++ fish_greeting.fish
@@ -0,0 +1,9 @@
+function fish_greeting
+  echo
+  for c in black brblack blue brblue green brgreen cyan brcyan red brred magenta brmagenta yellow bryellow white brwhite
+  set_color $c
+    echo -n " "
+  end
+  echo
+end
+
