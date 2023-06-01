8a136077119e7e8243f9c0e2281707bcc46bf554
Fantastic Mr. Fox <87468088+gadefox@users.noreply.github.com>
2023-06-01 17:49:39 +0200

    Add files via upload

---
+++ nvim/mail.lua
@@ -0,0 +1,7 @@
+vim.opt_local.comments:append("nb:>")
+vim.opt_local.fo:append("aw")
+vim.opt_local.textwidth = 72
+vim.opt_local.spell = true
+vim.opt_local.spelllang = "en_us"
+
+--vim.keymap.set("n", "<leader>x", "ZZ", { silent = true })
