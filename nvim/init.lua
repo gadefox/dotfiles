84499208a836a0b067355f377b2d51c3fd22f909
Fantastic Mr. Fox <87468088+gadefox@users.noreply.github.com>
2023-06-01 17:35:32 +0200

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,10 +6,7 @@ require("packer").startup(function(use)
   use "HiPhish/nvim-ts-rainbow2"
   use "windwp/nvim-autopairs"
   use "numToStr/Comment.nvim"
---  use { "akinsho/bufferline.nvim", requires = "nvim-tree/nvim-web-devicons" }
   use { "utilyre/barbecue.nvim", requires = { "nvim-tree/nvim-web-devicons", "SmiteshP/nvim-navic" } }
--- use "tamton-aquib/staline.nvim"
--- use "nvim-neo-tree/neo-tree.nvim"
   use "nvim-tree/nvim-tree.lua"
   use { "folke/noice.nvim", requires = { "rcarriga/nvim-notify", "MunifTanjim/nui.nvim" } }
   use "folke/which-key.nvim"
@@ -21,9 +18,7 @@ require("packer").startup(function(use)
   use { "hrsh7th/nvim-cmp", requires = { "hrsh7th/cmp-nvim-lsp", "hrsh7th/cmp-buffer", "hrsh7th/cmp-cmdline", "hrsh7th/cmp-path" } }
   use "neovim/nvim-lspconfig"
   use "DNLHC/glance.nvim"
-  use "stevearc/aerial.nvim"
   use "stevearc/oil.nvim"
--- use { "rcarriga/nvim-dap-ui", requires = { "mfussenegger/nvim-dap", "mfussenegger/nvim-dap-python", "jbyuki/one-small-step-for-vimkind" } }
 end)
 
 -- global settings
@@ -45,6 +40,7 @@ vim.o.pumheight = 20  -- max popup menu height
 vim.o.scrolloff = 10  -- min nr of lines to keep above and below the cursor
 vim.o.sidescrolloff = 15
 vim.o.shiftwidth = 2
+vim.o.shortmess = vim.o.shortmess .. "I"
 vim.o.softtabstop = 2
 vim.o.tabstop = 2
 vim.o.writebackup = false
@@ -58,6 +54,8 @@ vim.wo.wrap = false
 vim.cmd("filetype plugin indent on")
 
 vim.keymap.set("n", "<del>", "\"_x")
+vim.keymap.set("n", "<Tab>", ":bnext<CR>", { silent = true })
+vim.keymap.set("n", "<S-Tab>", ":bprevious<CR>", { silent = true })
 
 vim.api.nvim_create_autocmd("BufReadPost", {  -- go to last location when opening a buffer
   callback = function()
@@ -85,7 +83,9 @@ catppuccin.setup {
   gitgutter = false,
   gitsigns = false,
   leap = false,
-  neotree = { enabled = false },
+  neotree = {
+    enabled = false
+  },
   dashboard = false,
   neogit = false,
   barbar = false,
@@ -94,11 +94,6 @@ catppuccin.setup {
 }
 
 catppuccin.load("macchiato")
-
--- aerial ~ code outline window
-require("aerial").setup {
-  backends = { "treesitter", "lsp" }
-}
 
 -- autopairs package
 require("nvim-autopairs").setup()
@@ -113,59 +108,49 @@ require("barbecue").setup {
 
 local barbecue_ui = require("barbecue.ui")
 
-vim.api.nvim_create_autocmd({ "WinResized", "BufWinEnter", "CursorHold", "InsertLeave", "BufModifiedSet" }, {
+vim.api.nvim_create_autocmd({
+  "WinResized", "BufWinEnter", "CursorHold", "InsertLeave", "BufModifiedSet"
+}, {
   group = vim.api.nvim_create_augroup("barbecue.updater", {}),
   callback = function() barbecue_ui.update() end
 })
 
--- bufferline
--- vim.opt.showtabline = 2
-
--- mkey("n", "<Tab>", ":BufferLineCycleNext<CR>", { noremap = true, silent = true })
--- mkey("n", "<S-Tab>", ":BufferLineCyclePrev<CR>", { noremap = true, silent = true })
-
--- require("bufferline").setup {
---   options = {
---     show_close_icon = false,
---     buffer_close_icon = "",
---     right_mouse_command = "vert sbuffer %d",
---     offsets = { {
---       filetype = "NvimTree",
---       text = " File Explorer"
---     } }
---   },
---   highlights = {
--- --    indicator_selected = { fg = "#dc461d" }
---   }
--- }
-
 -- cmp
 local lsp_icons = {
-  Class = "󰠱",
-  Color = "",
-  Constant = "󰏿",
-  Constructor = "",
-  Enum = "練",
-  EnumMember = "",
-  Event = "",
-  Field = "",
-  File = "",
-  Folder = "",
-  Function = "",
-  Interface = "",
-  Keyword = "",
-  Method = "󰆧",
-  Module = "",
-  Operator = "",
-  Property = "",
-  Reference = "󰈇",
-  Snippet = "",
-  Struct = "󰙅",
-  Text = "",
-  TypeParameter = "",
-  Unit = "",
-  Value = "",
-  Variable = "󰀫"
+  Array = "󰅪 ",
+  Boolean = "◩ ",
+  Class = "󰠱 ",
+  Color = " ",
+  Constant = "󰏿 ",
+  Constructor = " ",
+  Enum = "練 ",
+  EnumMember = " ",
+  Event = " ",
+  Field = " ",
+  File = " ",
+  Folder = " ",
+  Function = " ",
+  Interface = " ",
+  Key = "󰌋 ",
+  Keyword = " ",
+  Method = "󰆧 ",
+  Module = " ",
+  Namespace = "󰌗 ",
+  Null = "󰟢 ",
+  Number = "󰎠 ",
+  Object = "󰅩 ",
+  Operator = " ",
+  Package = " ",
+  Property = " ",
+  Reference = "󰈇 ",
+  Snippet = " ",
+  String = " ",
+  Struct = "󰙅 ",
+  Text = " ",
+  TypeParameter = " ",
+  Unit = " ",
+  Value = " ",
+  Variable = "󰀫 "
 }
 
 local context = require("cmp.config.context")
@@ -203,7 +188,9 @@ cmp.setup.cmdline({ "/", "?" }, {
   sources = {
     {
       name = "buffer",
-      option = { keyword_pattern = [[\k\+]] }
+      option = {
+        keyword_pattern = [[\k\+]]
+      }
     }
   }
 })
@@ -221,43 +208,10 @@ require("colorizer").setup()
 
 -- comments
 require("Comment").setup {
-  mappings = { extra = false }
+  mappings = {
+    extra = false
+  }
 }
-
--- dap
--- local dapui = require("dapui")
--- dapui.setup()
-
--- local dap = require("dap")
--- dap.listeners.after.event_initialized["dapui_config"] = function()
---   dapui.open()
--- end
--- dap.listeners.before.event_terminated["dapui_config"] = function()
---   dapui.close()
--- end
--- dap.listeners.before.event_exited["dapui_config"] = function()
---   dapui.close()
--- end
-
--- debug lua
--- dap.configurations.lua = {
---   {
---     type = "nlua",
---     request = "attach",
---     name = "Attach to running Neovim instance"
---   }
--- }
---
--- dap.adapters.nlua = function(callback, config)
---   callback {
---     type = "server",
---     host = config.host or "127.0.0.1",
---     port = config.port or 8086
---   }
--- end
-
--- debug python
--- require("dap-python").setup("/usr/bin/python3")
 
 -- indent blackline (┊.↴)
 vim.opt.list = true
@@ -276,10 +230,10 @@ require("indent_blankline").setup {
 }
 
 -- glance
-vim.keymap.set("n", "gC", ":Glance definitions<CR>")
-vim.keymap.set("n", "gR", ":Glance references<CR>")
-vim.keymap.set("n", "gT", ":Glance type_definitions<CR>")
-vim.keymap.set("n", "gI", ":Glance implementations<CR>")
+vim.keymap.set("n", "gC", ":Glance definitions<CR>", { silent = true })
+vim.keymap.set("n", "gR", ":Glance references<CR>", { silent = true })
+vim.keymap.set("n", "gT", ":Glance type_definitions<CR>", { silent = true })
+vim.keymap.set("n", "gI", ":Glance implementations<CR>", { silent = true })
 
 require("glance").setup()
 
@@ -295,7 +249,7 @@ for type, icon in pairs(signs) do
   })
 end
 
-local lspcap = require('cmp_nvim_lsp').default_capabilities()
+local lspcap = require("cmp_nvim_lsp").default_capabilities()
 local lspcfg = require("lspconfig")
 
 lspcfg.clangd.setup {
@@ -339,8 +293,12 @@ set_hl({
 })
 --
 require("noice").setup {
-  health = { checker = false },
-  popupmenu = { enabled = true }
+  health = {
+    checker = false
+  },
+  popupmenu = {
+    enabled = true
+  }
 }
 
 -- oil ~ edit files and directories using the buffer
@@ -349,26 +307,6 @@ require("oil").setup {
     show_hidden = true
   }
 }
-
--- staline
--- vim.opt.laststatus = 2
---
--- local perc_chars = { " ", "▁", "▂", "▃", "▄", "▅", "▆", "▇", "█" }
---
--- local function percentage()
---   local index = vim.fn.line(".") / vim.fn.line("$") * #perc_chars
---   return "%#DoubleSep#" .. perc_chars[math.ceil(index)]
--- end
---
--- require("staline").setup {
---   defaults = { full_path = true },
---   sections = {
---     left = { "mode", "branch" },
---     right = { "lsp", percentage }
---   },
--- --  mode_colors = { c = "#EBBCBA", i = "#C4A7E7", v = "#569CD6" },
---   lsp_symbols = signs
--- }
 
 -- telescope
 set_hl({
@@ -392,71 +330,43 @@ telescope.setup {
     prompt_prefix = "   ",
     selection_caret = "  ",
     layout_config = {
-      horizontal = { prompt_position = "top" }
+      horizontal = {
+        prompt_position = "top"
+      }
     }
   }
 }
 
-telescope.load_extension("aerial")
 telescope.load_extension("fzf")
 
 -- tree
 require("nvim-tree").setup {
-  git = { ignore = false },
-  open_on_tab = true,
-  renderer = {
-    indent_markers = { enable = true }
+  git = {
+    ignore = false
   },
-  view = { width = 25 }
+  renderer = {
+    indent_markers = {
+      enable = true
+    }
+  },
+  view = {
+    width = 25
+  }
 }
 
--- set_hl({
---   NeoTreeFloatBorder = { bg = palette.base, fg = palette.base },
---   NeoTreeFloatTitle = { bg = palette.pink, fg = palette.mantle },
---   NeoTreeTabActive = { fg = palette.pink }
--- })
---
--- require("neo-tree").setup {
---   default_component_configs = {
---     modified = {
---       symbol = "●"
---     },
---     icon = {
---       default = "",
---       folder_closed = "",
---       folder_open = ""
---     },
---   },
---   default_source = "buffers",
---   filesystem = {
---     filtered_items = {
---       hide_dotfiles = false
---     }
---   },
---   hide_root_node = true,
---   popup_border_style = "solid",
---   -- source_selector = {
---   --   sources = {
---   --     { display_name = "  ", source = "filesystem" },
---   --     { display_name = "  󰭸", source = "buffers" },
---   --     { display_name = "  ", source = "git_status" }
---   --   },
---   --   statusline = true
---   -- },
---   window = {
---     width = 20
---   }
--- }
---
 -- treesitter
 require("nvim-treesitter.configs").setup {
   ensure_installed = { "c", "cpp", "lua", "python", "json" },
-  highlight = { enable = true },
-  rainbow = { enable = true },
+  highlight = {
+    enable = true
+  },
+  rainbow = {
+    enable = true
+  },
   textobjects = {
     lsp_interop = {
       enable = true,
-      border = 'none',
+      border = "none",
       floating_preview_opts = {},
       peek_definition_code = {
         ["<leader>sf"] = "@function.outer"
@@ -501,25 +411,10 @@ wkey.setup {
 }
 
 wkey.register({
-  a = { ":AerialToggle<CR>", "Aerial" },
-  b = { ":Navbuddy<CR>", "Navbuddy" },
   c = { ":bdelete<CR>", "Close Buffer" },
   e = { ":NvimTreeToggle<CR>", "Explorer" },
-  o = { ":Oil<CR>", "Oil" },
-  q = { ":q!<CR>", "Quit" },
-  w = { ":w!<CR>", "Save" },
-  s = {
-    name = "LSP",
-    c = { ":lua vim.lsp.buf.definition()<CR>", "Go to definition" },
-    d = { ":lua vim.lsp.buf.declaration()<CR>", "Go to declaration" },
-    i = { ":lua vim.lsp.buf.implementation()<CR>", "Go to implementation" },
-    ["?"] = { ":LspInfo<CR>", "Info" },
-    h = { ":lua vim.lsp.buf.hover()<CR>", "Hover" },
-    r = { ":lua vim.lsp.buf.rename()<CR>", "Rename" }
-  },
-  t = {
+  f = {
     name = "Telescope",
-    a = { ":Telescope aerial<CR>", "Aerial" },
     b = { ":Telescope buffers<CR>", "Buffers" },
     c = { ":Telescope commands<CR>", "Commands" },
     d = { ":Telescope diagnostics<CR>", "Diagnostics" },
@@ -528,5 +423,19 @@ wkey.register({
     h = { ":Telescope help_tags<CR>", "Help tags" },
     l = { ":Telescope highlights<CR>", "Highlights" },
     v = { ":Telescope vim_options<CR>", "Vim options" }
-  }
-}, { prefix = "<leader>" })
+  },
+  o = { ":Oil<CR>", "Oil" },
+  q = { ":q<CR>", "Quit" },
+  s = {
+    name = "LSP",
+    ["?"] = { ":LspInfo<CR>", "Info" },
+    c = { ":lua vim.lsp.buf.definition()<CR>", "Go to definition" },
+    d = { ":lua vim.lsp.buf.declaration()<CR>", "Go to declaration" },
+    i = { ":lua vim.lsp.buf.implementation()<CR>", "Go to implementation" },
+    h = { ":lua vim.lsp.buf.hover()<CR>", "Hover" },
+    r = { ":lua vim.lsp.buf.rename()<CR>", "Rename" }
+  },
+  w = { ":w<CR>", "Save" }
+}, {
+  prefix = "<leader>"
+})
