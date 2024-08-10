df0c7ed6bb983abc6e783ec5c8921ee0680c528d
Fantastic Mr. Fox <87468088+gadefox@users.noreply.github.com>
2024-08-10 15:09:43 +0200

    Update env.fish

--- fish/env.fish
+++ fish/env.fish
@@ -1,9 +1,1 @@
-set --path PATH "/usr/local/bin:/usr/bin"
-
-set --export XDG_CACHE_HOME "$HOME/.cache"
-set --export XDG_CONFIG_HOME "$HOME/.config"
-set --export XDG_DATA_HOME "$HOME/.local/share"
-set --export XDG_DATA_DIRS "/usr/local/share:/usr/share"
-
-set --export NNN_PLUG "d:dragon;i:imgview;p:preview"
 set --export MANPAGER "nvim +Man!"
