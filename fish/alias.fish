4190dc45d263125385b4a1734483322069df35b6
Fantastic Mr. Fox <87468088+gadefox@users.noreply.github.com>
2024-11-26 11:39:41 +0100

    Update alias.fish

--- fish/alias.fish
+++ fish/alias.fish
@@ -1,6 +1,8 @@
 alias c="clear"
 alias e="exit"
+alias irssi="irssi --home=~/.local/share/irssi"
 alias ls="ls --color=auto"
 alias l="ls -A"
 alias ll="ls -lA"
 alias mc="mc --xterm"
+alias unset="set --erase"
