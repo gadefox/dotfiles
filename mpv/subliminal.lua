217f3677406448809162639afcc87d711342b270
Fantastic Mr. Fox <87468088+gadefox@users.noreply.github.com>
2025-04-16 08:49:25 +0200

    Add files via upload

---
+++ mpv/subliminal.lua
@@ -0,0 +1,23 @@
+local utils = require "mp.utils"
+
+function subliminal()
+  mp.osd_message("Searching subtitle..")
+
+  local path = mp.get_property("path")
+  local exec = {
+    args = {
+      "subliminal", "download", "--single", "--force", "--language=en",
+      path
+    }
+  }
+
+  local res = utils.subprocess(exec)
+  if res.error == nil then
+    local srt = string.gsub(path, "%.%w+$", ".srt")
+    if mp.commandv("sub_add", srt) then
+      mp.osd_message("Subtitle download succeeded.")
+    end
+  end
+end
+
+mp.add_key_binding("b", "subliminal", subliminal)
