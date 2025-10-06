aa5b02030769e0caffe98397e6e6821e26a929cc
gadefox <gade@gmx.us>
2025-10-06 17:41:32 +0200

    Update all changes

--- mpv/scripts/srt.lua
+++ mpv/scripts/srt.lua
@@ -1,31 +1,34 @@
-function name()
-  local name = mp.get_property("filename")
+function srtname(s)
+  local patterns = { "%.%w+$", "%b[]", "AAC", "BONE", "HEVC", "MeGusta", "WEB%-DL", "WEBRip", "x264", "x265", "720p" }
+
+  for _, p in ipairs(patterns) do
+    s = string.gsub(s, p, "")
+  end
   
-  name = string.gsub(name, "%.%w+$", "")
-  name = string.gsub(name, "%-%[YTS%.MX%]$", "")
-  name = string.gsub(name, "%.AAC", "")
-  name = string.gsub(name, "%.x264", "")
-  name = string.gsub(name, "%.WEBRip", "")
-  name = string.gsub(name, "%.720p", "")
-  name = string.gsub(name, "[._]", " ")
-
-  return name
+  s = string.gsub(s, "[._-]", " ")
+  s = string.gsub(s, "%s+", " ")
+  return string.match(s, "^%s*(.-)%s*$")
 end
 
-local function srt()
-  mp.command_native_async({
-    name = "subprocess",
-    capture_stdout = true,
-    args = { "srt", name() }
-  },
-  function(success, result, _)
-    if success then
-      if mp.commandv("sub_add", result.stdout) then
-        local sub = string.gsub(result.stdout, "^.*()/", "")
-        mp.osd_message("Subtitle " .. sub .. " has been added.", 3)
+if mp ~= nil then
+  local function srtadd()
+    local name = mp.get_property("filename")
+    name = srtname(name);
+
+    mp.command_native_async({
+      name = "subprocess",
+      capture_stdout = true,
+      args = { "srt", name }
+    },
+    function(success, result, _)
+      if success then
+        if mp.commandv("sub_add", result.stdout) then
+          local sub = string.gsub(result.stdout, "^.*()/", "")
+          mp.osd_message("Subtitle " .. sub .. " has been added.", 3)
+        end
       end
-    end
-  end)
-end
+    end)
+  end
 
-mp.add_key_binding("V", "srt", srt)
+ mp.add_key_binding("V", "srt", srtadd)
+end
